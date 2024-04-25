b9d839be6303f195a1fb825964c4aa2a6cc01658
Rafal Rowniak <rrowniak@gmail.com>
2024-04-25 12:00:18 +0200

    Dynamic tab loading added

--- web/onload.js
+++ web/onload.js
@@ -1,6 +1,34 @@
-window.onload = function () {
-  // eslint-disable-next-line no-undef
-  diagOnLoad()
-  // eslint-disable-next-line no-undef
-  keyGenOnLoad()
+function load_tab(name, file, fn = null) {
+  var content;
+  $.get("web/" + file, function (data) {
+    content = data;
+    $(name).prepend(content);
+    if (fn) {
+      fn();
+    }
+  });
 }
+function my_onload() {
+  load_tab("#myTabContent", "tab_diags.html", function (response, status) {
+    // eslint-disable-next-line no-undef
+    diagOnLoad();
+  });
+  load_tab("#myTabContent", "tab_keygen.html", function (response, status) {
+    // eslint-disable-next-line no-undef
+    keyGenOnLoad();
+  });
+  load_tab("#myTabContent", "tab_x509val.html");
+  load_tab("#myTabContent", "tab_selfsign.html");
+  load_tab("#myTabContent", "tab_casign.html");
+  load_tab("#myTabContent", "tab_crl.html");
+  load_tab("#myTabContent", "tab_tls.html");
+}
+
+jQuery(document).ready(function () {
+  my_onload();
+});
+
+window.onload = function () {
+  // onload()
+  // console.log("onload");
+};
